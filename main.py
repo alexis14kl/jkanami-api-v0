f11b998e60aeda810495ee328ab30308bea76662
alexis14kl <rapalexism@gmai.com>
2025-02-28 10:51:02 -0500

    3

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # pip install fastapi uvicorn
+
+# ejecutar api
+# uvicorn main:app --reload
+
+
 from typing import Union
 from fastapi import FastAPI
 import re
@@ -11,21 +16,24 @@ import time
 
 app = FastAPI()
 
+# Configurar el navegador en modo headless
 def configurar_navegador():
     options = Options()
-
- options.add_argument("--headless")  # Ejecutar en modo headless
- 
+    options.add_argument("--headless")  # Ejecutar en modo headless
+    
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
+# Función para obtener el link m3u8
 def obtener_m3u8_link(driver, url):
     driver.get(url)
-    time.sleep(5)
+    time.sleep(5)  # Agregar un pequeño retraso para que la página se cargue correctamente
 
+    # Encontrar el iframe y cambiar el contexto
     iframe = driver.find_element(By.CLASS_NAME, "player_conte")
     driver.switch_to.frame(iframe)
 
+    # Obtener el código HTML del iframe y buscar el link m3u8
     iframe_html = driver.page_source
     match = re.search(r"var\s+parts\s*=\s*{[^}]*?swarmId\s*:\s*'([^']+\.m3u8)'", iframe_html)
 
@@ -34,6 +42,7 @@ def obtener_m3u8_link(driver, url):
     else:
         return None
 
+# Ruta para obtener el enlace m3u8
 @app.get("/get_m3u8/")
 def get_m3u8(url: str):
     driver = configurar_navegador()
@@ -45,11 +54,8 @@ def get_m3u8(url: str):
     else:
         return {"error": "No se encontró el enlace m3u8 en la página proporcionada."}
 
-
+# Ruta de prueba
 @app.get("/")
 def read_root():
-    return {"Hello": "Estas conectado"}
-# ejecutar api
-# uvicorn main:app --reload
-
+    return {"Hello": "Estás conectado"}
 
