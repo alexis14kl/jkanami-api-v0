e4d04756107009011aa3c44ebf2434dfcc1d7744
alexis14kl <rapalexism@gmai.com>
2025-03-01 00:34:11 -0500

    d

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
-# Establecer opciones de Chrome
-chrome_options = Options()
-chrome_options.add_argument('--headless')  # Ejecutar Chrome sin cabeza
-chrome_options.add_argument('--no-sandbox')  # Evitar problemas en entornos sin cabeza
-chrome_options.add_argument('--disable-dev-shm-usage')  # Solucionar errores de memoria
-chrome_options.add_argument('----disable-gpu')
-chrome_options.add_argument('--remote-debugging-port=9222')  # Puerto para depuración remota
+driver = webdriver.Chrome()
 
-# Especifica el servicio y el path del driver
-chrome_service = Service(executable_path="/app/.heroku/chrome/bin/google-chrome")
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-# Iniciar el navegador
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+title = driver.title
 
-# Accede a la página deseada
-driver.get("https://jkanime.net/dragon-ball-gt/16/")
+driver.implicitly_wait(0.5)
 
-# Espera un poco para asegurarse de que la página se cargue
-driver.implicitly_wait(10)
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-# Verifica si la página se ha cargado correctamente (comprobando el título)
-if "Dragon Ball GT" in driver.title:
-    print("La página se cargó correctamente")
-else:
-    print("La página no se cargó correctamente")
+text_box.send_keys("Selenium")
+submit_button.click()
 
-# Cierra el navegador
+message = driver.find_element(by=By.ID, value="message")
+text = message.text
+
 driver.quit()
