89cfcec6ec51d7e55c6f16bcc0cfafaa7022669e
alexis14kl <rapalexism@gmai.com>
2025-02-28 23:06:18 -0500

    w

--- main.py
+++ main.py
@@ -30,47 +30,6 @@ def configurar_navegador():
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
-def obtener_m3u8_link(driver, url):
-    # Acceder a la página principal
-    driver.get(url)
-
-    # Esperar a que la página cargue completamente
-    time.sleep(5)  # Ajusta el tiempo si es necesario (espera a que cargue el video)
-    
-    # Cambiar al iframe que contiene el reproductor
-    try:
-        iframe = driver.find_element(By.CLASS_NAME, "player_conte")
-        driver.switch_to.frame(iframe)
-    except Exception as e:
-        return {"error": f"No se encontró el iframe adecuado: {str(e)}"}
-
-    # Obtener el código fuente dentro del iframe
-    iframe_html = driver.page_source
-
-    # Buscar el script que contiene 'var parts =' con una expresión regular
-    match = re.search(r"var\s+parts\s*=\s*{[^}]*?swarmId\s*:\s*'([^']+\.m3u8)'", iframe_html)
-
-    if match:
-        # Si encontramos la URL, la retornamos
-        m3u8_url = match.group(1)
-        return {"m3u8_url": m3u8_url}
-    else:
-        return {"error": "No se encontró el enlace m3u8 dentro del iframe."}
-
-@app.get("/get-m3u8/")
-async def get_m3u8_link(url: str):  # Aquí cambiamos para recibir el parámetro en la query string
-    # Configurar el navegador
-    driver = configurar_navegador()
-
-    # Obtener la URL del m3u8
-    response = obtener_m3u8_link(driver, url)
-
-    # Cerrar el navegador después de la ejecución
-    driver.quit()
-
-    # Retornar la respuesta
-    return response
-
 @app.get("/")
 def read_root():
     return {"message": "¡Hola, FastAPI!"}
